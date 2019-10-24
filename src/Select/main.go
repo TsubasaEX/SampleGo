accd2c42a4ac1e2f3f560fe2105ad09c7262e949
zach chih <zach.chih@advantech.com.tw>
2019-10-24 14:03:38 +0800

    up

--- src/Select/main.go
+++ src/Select/main.go
@@ -3,12 +3,14 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 
 	ch := make(chan string)
 	ch2 := make(chan string)
+	ch3 := make(chan string, 3)
 
 	go func() {
 		ch <- "Hello"
@@ -18,12 +20,25 @@ func main() {
 		ch2 <- "World"
 	}()
 
+	go func() {
+		for i := 0; i < 3; i++ {
+			ch3 <- "fuck-" + strconv.Itoa(i)
+		}
+	}()
+
 	for i := 0; i < 2; i++ {
 		select {
 		case msg := <-ch:
 			fmt.Println(msg)
 		case msg2 := <-ch2:
 			fmt.Println(msg2)
+		}
+	}
+
+	for i := 0; i < 3; i++ {
+		select {
+		case msg3 := <-ch3:
+			fmt.Println(msg3)
 		}
 	}
 }
