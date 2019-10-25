2ce8e9e3fbe37695aee60bfe1ede7e49f3447439
zach chih <zach.chih@advantech.com.tw>
2019-10-25 13:59:25 +0800

    ConcurrencyNewsAgg

--- src/ConcurrencyNewsAgg/main.go
+++ src/ConcurrencyNewsAgg/main.go
@@ -6,10 +6,13 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
 	"text/template"
 )
 
 const url = "https://www.washingtonpost.com/news-sitemaps/index.xml"
+
+var wg sync.WaitGroup
 
 type NewsLocation struct {
 	Location []string `xml:"sitemap>loc"`
@@ -31,6 +34,18 @@ type NewsInfo struct {
 	Map   map[string]NewsMap
 }
 
+func retrieveRes(ch chan News, loc string) {
+	defer wg.Done()
+	loc = strings.Replace(loc, "\n", "", -1)
+	resp, _ := http.Get(loc)
+	bContent, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	var news News
+	xml.Unmarshal(bContent, &news)
+	ch <- news
+}
+
 func NewsAggHandle(w http.ResponseWriter, r *http.Request) {
 
 	resp, _ := http.Get(url)
@@ -39,20 +54,20 @@ func NewsAggHandle(w http.ResponseWriter, r *http.Request) {
 	var newslocation NewsLocation
 	xml.Unmarshal(bContent, &newslocation)
 
+	queue := make(chan News, len(newslocation.Location))
 	newsMap := make(map[string]NewsMap)
 	for _, loc := range newslocation.Location {
-		loc = strings.Replace(loc, "\n", "", -1)
-		resp, _ := http.Get(loc)
-		bContent, _ := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
+		wg.Add(1)
+		go retrieveRes(queue, loc)
+	}
+	wg.Wait()
+	close(queue)
 
-		var news News
-		xml.Unmarshal(bContent, &news)
-		for idx, _ := range news.Keywords {
-			newsMap[news.Titles[idx]] = NewsMap{news.Keywords[idx], news.Locations[idx]}
+	for elem := range queue {
+		for idx, _ := range elem.Keywords {
+			newsMap[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
 		}
 	}
-
 	newsInfo := NewsInfo{"Welcome to Z's News", newsMap}
 	t, _ := template.ParseFiles("basic.html")
 	fmt.Println(t.Execute(w, newsInfo))
@@ -65,6 +80,5 @@ func IndexHandle(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/agg", NewsAggHandle)
 	http.HandleFunc("/", IndexHandle)
-	http.ListenAndServe(":8080", nil)
-
+	http.ListenAndServe(":8081", nil)
 }
