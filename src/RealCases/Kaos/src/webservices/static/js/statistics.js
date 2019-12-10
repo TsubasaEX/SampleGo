ad31357f303f2702481ddb2535151c9f46f794c2
zach chih <zach.chih@advantech.com.tw>
2019-12-10 16:34:12 +0800

    up

--- src/RealCases/Kaos/src/webservices/static/js/statistics.js
+++ src/RealCases/Kaos/src/webservices/static/js/statistics.js
@@ -1,12 +1,53 @@
+var passPoints =[];
+var failPoints =[];
+
 $(document).ready( function () {
 	$('#statsTable > tbody > tr').each(function(index, tr) { 
-		$(this).find('td').each(function(index, td) {
-			console.log(index);
-			console.log(td);
-		});
+		var name=$(this).find('td:eq(1)').html();
+		var pass=$(this).find('td:eq(2)').html();
+		var fail=$(this).find('td:eq(3)').html();
+		var passObj={
+			y:parseInt(pass,10),
+			label:name
+		}
+		var failObj={
+			y:parseInt(fail,10),
+			label:name
+		}
+		passPoints.push(passObj);
+		failPoints.push(failObj);
+//		$(this).find('td').each(function(index, td) {
+//			switch(index){
+//				case 0:
+//					$(this).attr("id","no");
+//					break;
+//				case 1:
+//					$(this).attr("id","name");
+//					var name = $(this).find("#name").html();
+//					map.set("label",name);
+//					console.log(map);
+//					passPoints.push(map) 
+//					break;
+//				case 2:
+//					$(this).attr("id","pass");
+//					break;
+//				case 3:
+//					$(this).attr("id","fail");
+//					break;
+//				case 4:
+//					$(this).attr("id","total");
+//					break;
+//				case 5:
+//					$(this).attr("id","passRate");
+//					break;
+//				case 6:
+//					$(this).attr("id","ts");
+//					break;
+//			}
+//		});
 	});
-	
-      $('#statsTable').DataTable();
+
+    $('#statsTable').DataTable();
 } );
 
 //window.onload = function(){
@@ -14,15 +55,8 @@ $(document).ready( function () {
 //};
 
 $(window).on('load', function () {
-	 
-//	$('#statsTable > tbody > tr').each(function(index, tr) { 
-////		alert(this);
-//		var name = $(this).find('#name').html();
-//  		console.log(index);
-//// 		console.log(name);
-//		console.log(tr);
-//	});
-	
+	console.log(passPoints);
+	console.log(failPoints);
 	//Better to construct options first and then pass it as a parameter
 	var options = {
 		animationEnabled: true,
@@ -44,67 +78,15 @@ $(window).on('load', function () {
 			type: "stackedBar100",
 			toolTipContent: "{label}<br><b>{name}:</b> {y} (#percent%)",
 			showInLegend: true, 
-			name: "April",
-			dataPoints: [
-				{ y: 550, label: "Water Filter" },
-				{ y: 450, label: "Modern Chair" },
-				{ y: 70, label: "VOIP Phone" },
-				{ y: 200, label: "Microwave" },
-				{ y: 70, label: "Water Filter" },
-				{ y: 324, label: "Expresso Machine" },
-				{ y: 300, label: "Lobby Chair" },
-		{ y: 500, label: "Lobby Chair1" },
-		{ y: 500, label: "Lobby Chair2" },
-		{ y: 500, label: "Lobby Chair3" },
-		{ y: 500, label: "Lobby Chair4" },
-		{ y: 500, label: "Lobby Chair5" },
-		{ y: 500, label: "Lobby Chair6" },
-		{ y: 500, label: "Lobby Chair7" }
-			]
+			name: "Pass",
+			dataPoints: passPoints
 		},
 		{
 			type: "stackedBar100",
 			toolTipContent: "<b>{name}:</b> {y} (#percent%)",
 			showInLegend: true, 
-			name: "May",
-			dataPoints: [
-				{ y: 450, label: "Water Filter" },
-				{ y: 550, label: "Modern Chair" },
-				{ y: 270, label: "VOIP Phone" },
-				{ y: 400, label: "Microwave" },
-				{ y: 270, label: "Water Filter" },
-				{ y: 524, label: "Expresso Machine" },
-				{ y: 500, label: "Lobby Chair" },
-		{ y: 500, label: "Lobby Chair1" },
-		{ y: 500, label: "Lobby Chair2" },
-		{ y: 500, label: "Lobby Chair3" },
-		{ y: 500, label: "Lobby Chair4" },
-		{ y: 500, label: "Lobby Chair5" },
-		{ y: 500, label: "Lobby Chair6" },
-		{ y: 500, label: "Lobby Chair7" }
-			]
-		}, 
-		{
-			type: "stackedBar100",
-			toolTipContent: "<b>{name}:</b> {y} (#percent%)",
-			showInLegend: true, 
-			name: "June",
-			dataPoints: [
-				{ y: 350, label: "Water Filter" },
-				{ y: 660, label: "Modern Chair" },
-				{ y: 265, label: "VOIP Phone" },
-				{ y: 271, label: "Microwave" },
-				{ y: 125, label: "Water Filter" },
-				{ y: 360, label: "Expresso Machine" },
-				{ y: 340, label: "Lobby Chair" },
-		{ y: 500, label: "Lobby Chair1" },
-		{ y: 500, label: "Lobby Chair2" },
-		{ y: 500, label: "Lobby Chair3" },
-		{ y: 500, label: "Lobby Chair4" },
-		{ y: 500, label: "Lobby Chair5" },
-		{ y: 500, label: "Lobby Chair6" },
-		{ y: 500, label: "Lobby Chair7" }
-			]
+			name: "Fail",
+			dataPoints: failPoints
 		}]
 	};
 	
